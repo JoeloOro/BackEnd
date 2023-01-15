eaf0edcc65dc6979e7dcfd699836e0dcda610da3
JoeloOro <joelarancibia137@gmail.com>
2023-01-15 06:56:05 -0300

    BackEnd/8

--- demo2/src/main/java/com/example/demo2/Controller/SkillController.java
+++ demo2/src/main/java/com/example/demo2/Controller/SkillController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:4200/"})
+@CrossOrigin(origins = {"https://frontendjoel.web.app/"})
 public class SkillController {
     @Autowired
    private ISkillService PersoServ;
